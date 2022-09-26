13470e9de89665f9d6ed7f540964b4eb00692eb4
Replit user <>
2022-09-26 01:35:28 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -170,7 +170,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
       result=tree->lower_than(key,aux->pair->key);
       paramenor=tree->lower_than(aux->pair->key,menor);
       menorkey=tree->lower_than(key,menor);
-      if (paramenor==1 && menorkey==1){
+      if (paramenor==1 && menorkey==1 && result==1){
         menor=aux->pair->key;
         menorpair=aux->pair;
       }
