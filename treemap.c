146462c7881d98de336284d2c473c71989929809
Replit user <>
2022-09-26 00:10:23 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -167,11 +167,10 @@ Pair * upperBound(TreeMap * tree, void* key) {
       result=tree->lower_than(key,aux->key);
       paramenor=tree->lower_than(menor,aux->key);
       if (result==1 && paramenor==0)
-        menor=aux->key;
+        return search;
       aux=nextTreeMap(tree);
     }
     search=searchTreeMap(tree,menor);
-    return search;
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
